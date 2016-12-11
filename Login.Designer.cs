9db2e3735ababf415838dc847090f1d94f34621c
Ian Norris <icstatic@gmail.com>
2016-12-11 19:45:55 +0000

    First revision with login box and SCP command to get config to local file.

---
+++ Login.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace vyatta_config_updater
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Address = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.OK = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Address, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Username, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Password, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(12);
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(496, 126);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(15, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Router IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(15, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(230, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(15, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(230, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password:";
+            // 
+            // Address
+            // 
+            this.Address.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Address.Location = new System.Drawing.Point(251, 15);
+            this.Address.Name = "Address";
+            this.Address.Size = new System.Drawing.Size(230, 20);
+            this.Address.TabIndex = 0;
+            // 
+            // Username
+            // 
+            this.Username.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Username.Location = new System.Drawing.Point(251, 41);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(230, 20);
+            this.Username.TabIndex = 1;
+            // 
+            // Password
+            // 
+            this.Password.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Password.Location = new System.Drawing.Point(251, 67);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(230, 20);
+            this.Password.TabIndex = 2;
+            this.Password.UseSystemPasswordChar = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.CausesValidation = false;
+            this.flowLayoutPanel1.Controls.Add(this.Cancel);
+            this.flowLayoutPanel1.Controls.Add(this.OK);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(251, 93);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(230, 100);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // OK
+            // 
+            this.OK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.OK.Location = new System.Drawing.Point(71, 3);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(75, 23);
+            this.OK.TabIndex = 3;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // Cancel
+            // 
+            this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Cancel.Location = new System.Drawing.Point(152, 3);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 23);
+            this.Cancel.TabIndex = 4;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(496, 126);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.Name = "Login";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Vyatta Config Builder - Router Login";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Address;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Button OK;
+    }
+}
+
---
+++ Login.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinSCP;
+
+namespace vyatta_config_updater
+{
+	public partial class Login : Form
+	{
+		const string RegistryKey = @"SOFTWARE\VyattaConfigUpdater";
+		string Fingerprint = "";
+
+		public Login()
+		{
+			InitializeComponent();
+
+			Microsoft.Win32.RegistryKey regSettings = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(RegistryKey);
+			Address.Text = (string)regSettings.GetValue("Address");
+			Username.Text = (string)regSettings.GetValue("Username");
+			Fingerprint = (string)regSettings.GetValue("Fingerprint");
+			regSettings.Close();
+
+			if( Username.Text.Length > 0 )
+			{
+				ActiveControl = Password;
+			}
+		}
+
+		private void Cancel_Click( object sender, EventArgs e )
+		{
+			Application.Exit();
+		}
+
+		private void OK_Click( object sender, EventArgs e )
+		{
+			SessionOptions options = new SessionOptions
+			{
+				Protocol = Protocol.Scp,
+				HostName = Address.Text,
+				UserName = Username.Text,
+				Password = Password.Text
+			};
+
+			using( Session session = new Session() )
+			{
+				if( Fingerprint == null || Fingerprint.Length == 0 )
+				{
+					Fingerprint = session.ScanFingerprint( options );
+				}
+				else
+				{
+					string NewFingerprint = session.ScanFingerprint( options );
+					if( Fingerprint != NewFingerprint )
+					{
+						var Result = MessageBox.Show( "Host fingerprint has changed from:\n" + Fingerprint + "\nTo:\n" + NewFingerprint + "\n\nDo you wish to continue?", "Fingerprint changed", MessageBoxButtons.YesNo, MessageBoxIcon.Warning );
+						if( Result == DialogResult.No )
+						{
+							return;
+						}
+
+						Fingerprint = NewFingerprint;
+					}
+				}
+
+				Microsoft.Win32.RegistryKey regSettings = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(RegistryKey);
+				regSettings.SetValue("Address", Address.Text);
+				regSettings.SetValue("Username", Username.Text);
+				regSettings.SetValue("Fingerprint", Fingerprint);
+				regSettings.Close();
+			
+				options.SshHostKeyFingerprint = Fingerprint;
+
+				try
+				{
+					session.Open( options );
+				}
+				catch( SessionException exception )
+				{
+					MessageBox.Show( exception.Message, "Error connecting", MessageBoxButtons.OK, MessageBoxIcon.Error );
+					return;
+				}
+
+				string tempConfigPath = Path.ChangeExtension(Path.GetTempFileName(), Guid.NewGuid().ToString());
+				
+				try
+				{
+					TransferOperationResult result = session.GetFiles( "/config/config.boot", tempConfigPath, false );
+					result.Check();
+				}
+				catch( SessionException exception )
+				{
+					MessageBox.Show( exception.Message, "Error connecting", MessageBoxButtons.OK, MessageBoxIcon.Error );
+					return;
+				}
+			}
+		}
+	}
+}
