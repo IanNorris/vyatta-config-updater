d2a3f2ce359a1ec9d2e929350cf5100e9b118aed
Ian Norris <icstatic@gmail.com>
2016-12-13 19:49:28 +0000

    Removed WinSCP dependency as SSH.NET can do both jobs.

--- Login.cs
+++ Login.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WinSCP;
+using Renci.SshNet;
+using Renci.SshNet.Common;
 
 namespace vyatta_config_updater
 {
@@ -52,71 +54,35 @@ namespace vyatta_config_updater
 
 		private void OK_Click( object sender, EventArgs e )
 		{
-			SessionOptions options = new SessionOptions
+			try
 			{
-				Protocol = Protocol.Scp,
-				HostName = Address.Text,
-				UserName = Username.Text,
-				Password = Password.Text
-			};
+				using( ScpClient client = new ScpClient( Address.Text, Username.Text, Password.Text ) )
+				{
+					client.Connect();
 
-			using( Session session = new Session() )
-			{
-				if( Fingerprint == null || Fingerprint.Length == 0 )
-				{
-					Fingerprint = session.ScanFingerprint( options );
-				}
-				else
-				{
-					string NewFingerprint = session.ScanFingerprint( options );
-					if( Fingerprint != NewFingerprint )
+					string tempConfigPath = Path.ChangeExtension(Path.GetTempFileName(), Guid.NewGuid().ToString());
+
+					using( Stream tempFile = new FileStream( tempConfigPath, FileMode.CreateNew ) )
 					{
-						var Result = MessageBox.Show( "Host fingerprint has changed from:\n" + Fingerprint + "\nTo:\n" + NewFingerprint + "\n\nDo you wish to continue?", "Fingerprint changed", MessageBoxButtons.YesNo, MessageBoxIcon.Warning );
-						if( Result == DialogResult.No )
-						{
-							return;
-						}
-
-						Fingerprint = NewFingerprint;
+						client.Download( "/config/config.boot", tempFile );
 					}
-				}
 
-				Microsoft.Win32.RegistryKey regSettings = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(RegistryKey);
-				regSettings.SetValue("Address", Address.Text);
-				regSettings.SetValue("Username", Username.Text);
-				regSettings.SetValue("Fingerprint", Fingerprint);
-				regSettings.Close();
-			
-				options.SshHostKeyFingerprint = Fingerprint;
+					client.Disconnect();
 
-				try
-				{
-					session.Open( options );
+					var MainForm = new Main( Address.Text, Username.Text, Password.Text, tempConfigPath );
+					Visible = false;
+					MainForm.Show();
+					ProgrammaticClosing = true;
+					Close();
 				}
-				catch( SessionException exception )
-				{
-					MessageBox.Show( exception.Message, "Error connecting", MessageBoxButtons.OK, MessageBoxIcon.Error );
-					return;
-				}
-
-				string tempConfigPath = Path.ChangeExtension(Path.GetTempFileName(), Guid.NewGuid().ToString());
-				
-				try
-				{
-					TransferOperationResult result = session.GetFiles( "/config/config.boot", tempConfigPath, false );
-					result.Check();
-				}
-				catch( SessionException exception )
-				{
-					MessageBox.Show( exception.Message, "Error connecting", MessageBoxButtons.OK, MessageBoxIcon.Error );
-					return;
-				}
-
-				var MainForm = new Main( Address.Text, Username.Text, Password.Text, tempConfigPath );
-				Visible = false;
-				MainForm.Show();
-				ProgrammaticClosing = true;
-				Close();
+			}
+			catch( SshException exception )
+			{
+				MessageBox.Show( exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+			}
+			catch( IOException exception )
+			{
+				MessageBox.Show( exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
 			}
 		}
 	}
